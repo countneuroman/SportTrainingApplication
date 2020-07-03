c9e0e8626d3d4b6ea0fe22570a06fb661700f3b2
Vadim Alekseev <olorinoren@gmail.com>
2020-07-03 15:22:47 +0300

    Create project

---
+++ SportTrainingApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportTrainingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
